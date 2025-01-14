c7d62c78c67892d97c4ccb81ba924af0643aca81
Soly01 <solimana425@yahoo.com>
2025-01-14 22:54:33 +0200

    finshing first part

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 @Component({
   selector: 'app-home-page',
-  imports: [ButtonModule, InputTextModule],
+  imports: [
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    AvatarModule,
+    AvatarGroupModule,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   encapsulation: ViewEncapsulation.None,
